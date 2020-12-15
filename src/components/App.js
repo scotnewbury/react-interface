6b636b3f4d014ad81365d559be5f1ab23260a70a
Scot Newbury <scot@scotnewbury.com>
2020-12-15 00:22:54 -0500

    course completed through 2.2

--- src/components/App.js
+++ src/components/App.js
@@ -4,22 +4,33 @@ import AddAppointments from './AddAppointments';
 import SearchAppointments from './SearchAppointments';
 import ListAppointments from './ListAppointments';
 
-function App() {
-  return (
-    <main className="page bg-white" id="petratings">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12 bg-white">
-            <div className="container">
-              <AddAppointments />
-              <SearchAppointments />
-              <ListAppointments />              
+class App extends Component {
+
+constructor() {
+    super();
+    this.state = {
+      myName: 'Scot'
+    }
+  }
+
+  render () {
+    return (
+      <main className="page bg-white" id="petratings">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 bg-white">
+              <div className="container">
+                { this.state.myName }
+                <AddAppointments />
+                <SearchAppointments />
+                <ListAppointments />              
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  );
+      </main>
+    );
+  };
 }
 
 export default App;
