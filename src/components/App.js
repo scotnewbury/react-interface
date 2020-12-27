bf515357cbbc87da548439eff52c0762e32a832e
Scot Newbury <scot@scotnewbury.com>
2020-12-27 17:50:00 -0500

    Course complete through 4.4

--- src/components/App.js
+++ src/components/App.js
@@ -16,16 +16,25 @@ class App extends Component {
       formDisplay: false,
       orderBy: 'petName',
       orderDir: 'asc',
+      queryText: '',
       lastIndex: 0
     }
     this.deleteAppointment = this.deleteAppointment.bind(this);
     this.toggleForm = this.toggleForm.bind(this);
     this.addAppointment = this.addAppointment.bind(this);
+    this.changeOrder = this.changeOrder.bind(this);
    }
 
   toggleForm() {
     this.setState({
       formDisplay: !this.state.formDisplay
+    });
+  }
+
+  changeOrder (order, dir) {
+    this.setState({
+      orderBy: order,
+      orderDir: dir
     });
   }
 
@@ -74,7 +83,7 @@ class App extends Component {
       order = -1;
     }
 
-    filteredApts.sort((a,b) => {
+    filteredApts = filteredApts.sort((a,b) => {
       if (a[this.state.orderBy].toLowerCase() <
         b[this.state.orderBy].toLowerCase()
       ) {
@@ -82,6 +91,19 @@ class App extends Component {
       } else {
         return 1 * order;
       }
+    })
+    .filter(eachItem => {
+      return (
+        eachItem['petName']
+        .toLowerCase()
+        .includes(this.state.queryText.toLocaleLowerCase()) ||
+        eachItem['ownerName']
+        .toLowerCase()
+        .includes(this.state.queryText.toLocaleLowerCase()) ||
+        eachItem['aptNotes']
+        .toLowerCase()
+        .includes(this.state.queryText.toLocaleLowerCase())
+      )
     });
 
 
@@ -98,6 +120,7 @@ class App extends Component {
                 <SearchAppointments 
                   orderBy = {this.state.orderBy}
                   orderDir = {this.state.orderDir}
+                  changeOrder = {this.changeOrder}
                 />
                 <ListAppointments 
                   appointments={filteredApts}
